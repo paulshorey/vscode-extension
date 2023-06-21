d036a0a768ce2246a42614c5122820719446e607
Paul Shorey <web@artspaces.net>
2023-06-21 15:17:57 -0500

    refine, add save functionality

--- out/mods/code_selected_sxToChakra.js
+++ out/mods/code_selected_sxToChakra.js
@@ -1,7 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const vscode = require("vscode");
-// console.log('registered command "convertcode.selectedToUpperCase"');
 exports.default = async () => {
     try {
         const editor = vscode.window.activeTextEditor;
@@ -10,53 +9,90 @@ exports.default = async () => {
         }
         let str = editor.document.getText(editor.selection);
         // CONVERT
-        str = str.replace(/sx={{([^}}]*)}}/g, function (match, str) {
-            console.log('match, str', [match, str]);
-            if (match.includes(`':`)) {
-                /*
-                 * ADVANCED SX OBJECT NOT YET SUPPORTED
-                 */
-                return match;
-            }
-            else {
-                /*
-                 * SIMPLE SX OBJECT -- REPLACE WITH CHAKRA PROPS
-                 */
-                // replace arrays `mb: ['24px', '36px', null, null]` with `mb={['24px', '36px']}`
-                str = str.replace(new RegExp(`(\\w+): ?\\[(.*?)],?`, 'g'), function (match, p1, p2) {
-                    console.log('before', p2);
-                    // remove trailing null values from array
-                    let arr = p2.split(',');
-                    console.log('arr', arr);
-                    if (arr?.length > 1) {
-                        while (arr[arr.length - 1].trim() === 'null') {
-                            arr.pop();
-                        }
-                        p2 = arr.join(',');
-                    }
-                    console.log('after', p2);
-                    // inject array items back into JSX prop
-                    return `${p1}={[${p2}]}`;
-                });
-                // replace color: 'white' with color="white"
-                str = str.replace(new RegExp(`(\\w+): ?'([^']+?)',?`, 'g'), '$1="$2"');
-                // replace pb:0 with pb={0}
-                str = str.replace(new RegExp(`(\\w+): ([0-9]+)`, 'g'), '$1={$2}');
-                // replace color: 'white' with color='white'
-                // str = str.replace(/: ?/g, '=');
-                // remove trailing comma
-                str = str.replace(/,\s*$/g, '');
-                str = str.replace(/,\s*\n/g, '\n');
-                return str;
-            }
-        });
-        /*
-         * REPLACE
-         */
+        if (str.includes('sx={{')) {
+            str = str.replace(/sx={{([^}}]*)}}/g, replaceSxObjectContent);
+        }
+        else {
+            str = replaceSxObjectContent('', str);
+        }
+        // REPLACE
         await editor.edit((editBuilder) => {
             editBuilder.replace(editor.selection, str);
         });
+        // SAVE
+        if (vscode.window.activeTextEditor) {
+            vscode.window.activeTextEditor.document.save();
+        }
+        else {
+            vscode.commands.executeCommand('workbench.action.files.save');
+        }
     }
     catch (err) { }
 };
+function replaceSxObjectContent(match, str) {
+    /*
+     * ADVANCED SX OBJECT -- ATTEMPT TO EVAL
+     */
+    if (match.includes(`':`)) {
+        console.log('detected "\':" - attempting to parse str', str);
+        try {
+            let str = '';
+            // let obj = JSON.parse('{' + str + '}');
+            let obj = eval(`({${str}})`);
+            console.log('obj', obj);
+            if (obj) {
+                str = buildSxStringFromObject(obj);
+            }
+            return str;
+        }
+        catch (e) { }
+    }
+    /*
+     * SIMPLE SX OBJECT -- REPLACE WITH CHAKRA PROPS
+     */
+    // replace array
+    str = str.replace(new RegExp(`(\\w+): ?\\[(.*?)],?`, 'g'), function (match, p1, p2) {
+        // console.log('before', p2);
+        // remove trailing null values
+        let arr = p2.split(',');
+        // console.log('arr', arr);
+        if (arr?.length > 1) {
+            while (arr[arr.length - 1].trim() === 'null') {
+                arr.pop();
+            }
+            p2 = arr.join(',');
+        }
+        // console.log('after', p2);
+        // inject array items back into JSX prop
+        return `${p1}={[${p2}]}`;
+    });
+    // replace color: 'white' with color="white"
+    str = str.replace(new RegExp(`(\\w+): ?'([^']+?)',?`, 'g'), '$1="$2"');
+    // replace pb:0 with pb={0}
+    str = str.replace(new RegExp(`(\\w+): ([0-9-]+)`, 'g'), '$1={$2}');
+    // remove trailing comma
+    str = str.replace(/,\s*$/g, '');
+    str = str.replace(/,\s*\n/g, '\n');
+    return str;
+}
+function buildSxStringFromObject(obj) {
+    let str = '';
+    for (let key in obj) {
+        let val = obj[key];
+        if (key === ':hover') {
+            key = '_hover';
+        }
+        if (key === ':focus') {
+            key = '_focus';
+        }
+        if (typeof val === 'string') {
+            val = val.replace(/"/g, '\\"');
+            str += `${key}="${val}" `;
+        }
+        else {
+            str += `${key}={${val}} `;
+        }
+    }
+    return str;
+}
 //# sourceMappingURL=code_selected_sxToChakra.js.map
--- src/mods/code_selected_sxToChakra.ts
+++ src/mods/code_selected_sxToChakra.ts
@@ -1,5 +1,4 @@
 import * as vscode from 'vscode';
-// console.log('registered command "convertcode.selectedToUpperCase"');
 
 export default async () => {
   try {
@@ -9,54 +8,89 @@ export default async () => {
     }
     let str = editor.document.getText(editor.selection);
     // CONVERT
-    str = str.replace(/sx={{([^}}]*)}}/g, function (match, str: string) {
-      console.log('match, str', [match, str]);
-      if (match.includes(`':`)) {
-        /*
-         * ADVANCED SX OBJECT NOT YET SUPPORTED
-         */
-        return match;
-      } else {
-        /*
-         * SIMPLE SX OBJECT -- REPLACE WITH CHAKRA PROPS
-         */
-        // replace arrays `mb: ['24px', '36px', null, null]` with `mb={['24px', '36px']}`
-        str = str.replace(
-          new RegExp(`(\\w+): ?\\[(.*?)],?`, 'g'),
-          function (match, p1, p2) {
-            console.log('before', p2);
-            // remove trailing null values from array
-            let arr = p2.split(',');
-            console.log('arr', arr);
-            if (arr?.length > 1) {
-              while (arr[arr.length - 1].trim() === 'null') {
-                arr.pop();
-              }
-              p2 = arr.join(',');
-            }
-            console.log('after', p2);
-            // inject array items back into JSX prop
-            return `${p1}={[${p2}]}`;
-          }
-        );
-        // replace color: 'white' with color="white"
-        str = str.replace(new RegExp(`(\\w+): ?'([^']+?)',?`, 'g'), '$1="$2"');
-        // replace pb:0 with pb={0}
-        str = str.replace(new RegExp(`(\\w+): ([0-9]+)`, 'g'), '$1={$2}');
-        // replace color: 'white' with color='white'
-        // str = str.replace(/: ?/g, '=');
-        // remove trailing comma
-        str = str.replace(/,\s*$/g, '');
-        str = str.replace(/,\s*\n/g, '\n');
-        return str;
-      }
-    });
-
-    /*
-     * REPLACE
-     */
+    if (str.includes('sx={{')) {
+      str = str.replace(/sx={{([^}}]*)}}/g, replaceSxObjectContent);
+    } else {
+      str = replaceSxObjectContent('', str);
+    }
+    // REPLACE
     await editor.edit((editBuilder) => {
       editBuilder.replace(editor.selection, str);
     });
+    // SAVE
+    if (vscode.window.activeTextEditor) {
+      vscode.window.activeTextEditor.document.save();
+    } else {
+      vscode.commands.executeCommand('workbench.action.files.save');
+    }
   } catch (err) {}
 };
+
+function replaceSxObjectContent(match: string, str: string) {
+  /*
+   * ADVANCED SX OBJECT -- ATTEMPT TO EVAL
+   */
+  if (match.includes(`':`)) {
+    console.log('detected "\':" - attempting to parse str', str);
+    try {
+      let str = '';
+      // let obj = JSON.parse('{' + str + '}');
+      let obj = eval(`({${str}})`);
+      console.log('obj', obj);
+      if (obj) {
+        str = buildSxStringFromObject(obj);
+      }
+      return str;
+    } catch (e) {}
+  }
+  /*
+   * SIMPLE SX OBJECT -- REPLACE WITH CHAKRA PROPS
+   */
+  // replace array
+  str = str.replace(
+    new RegExp(`(\\w+): ?\\[(.*?)],?`, 'g'),
+    function (match, p1, p2) {
+      // console.log('before', p2);
+      // remove trailing null values
+      let arr = p2.split(',');
+      // console.log('arr', arr);
+      if (arr?.length > 1) {
+        while (arr[arr.length - 1].trim() === 'null') {
+          arr.pop();
+        }
+        p2 = arr.join(',');
+      }
+      // console.log('after', p2);
+      // inject array items back into JSX prop
+      return `${p1}={[${p2}]}`;
+    }
+  );
+  // replace color: 'white' with color="white"
+  str = str.replace(new RegExp(`(\\w+): ?'([^']+?)',?`, 'g'), '$1="$2"');
+  // replace pb:0 with pb={0}
+  str = str.replace(new RegExp(`(\\w+): ([0-9-]+)`, 'g'), '$1={$2}');
+  // remove trailing comma
+  str = str.replace(/,\s*$/g, '');
+  str = str.replace(/,\s*\n/g, '\n');
+  return str;
+}
+
+function buildSxStringFromObject(obj: Record<string, any>) {
+  let str = '';
+  for (let key in obj) {
+    let val = obj[key];
+    if (key === ':hover') {
+      key = '_hover';
+    }
+    if (key === ':focus') {
+      key = '_focus';
+    }
+    if (typeof val === 'string') {
+      val = val.replace(/"/g, '\\"');
+      str += `${key}="${val}" `;
+    } else {
+      str += `${key}={${val}} `;
+    }
+  }
+  return str;
+}
